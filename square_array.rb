db3aac16bc1f9db5101e367085cb4588b3b25f9b
MJ Johnson <mjohnson4685@gmail.com>
2020-09-12 00:14:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
 
   array = [1,2,3]
 
-  array.each do |new_numbers|
-     puts "(#{new_numbers}**#{new_numbers})"
+  array.each do |number|
+     puts "(#{number}**2)"
 
    end
 end
