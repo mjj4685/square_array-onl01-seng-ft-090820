cab5b966015b2350dfd41507962774005480e074
MJ Johnson <mjohnson4685@gmail.com>
2020-09-12 00:53:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@
 
 def square_array(array)
    new_numbers = []
-  array.each { |num| num * num new_numbers.push(num * num) }
-
+  array.each { |num| new_numbers.push(num * num) }
+  new_numbers
 end
